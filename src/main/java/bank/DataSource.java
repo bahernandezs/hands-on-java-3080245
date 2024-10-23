e943763bfc300c7d2bd382bcd023e4fa1b4f5b21
bahernandezs <bryan.ahs@hotmail.com>
2024-10-23 11:08:51 -0500

    getCustomer and connection created

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,8 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -17,5 +19,34 @@ public class DataSource {
         }
 
         return connection;
+    }
+
+    public static Customer getCustomer(String username) {
+        String sql = "select * from customers where username = ?";
+        Customer customer = null;
+        try (
+            Connection cnn = connect();
+            PreparedStatement statement = cnn.prepareStatement(sql)
+        ) {
+            statement.setString(1, username);
+            try (ResultSet resultSet = statement.executeQuery();) {
+                customer = new Customer(
+                    resultSet.getInt("id"), 
+                    resultSet.getString("name"), 
+                    resultSet.getString("username"), 
+                    resultSet.getString("password"), 
+                    resultSet.getInt("account_id"));
+            }
+            
+        } catch (SQLException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return customer;
+    }
+
+    public static void main(String[] args) {
+        Customer customer = getCustomer("twest8o@friendfeed.com");
+        System.out.println(customer.getName());
     }
 }
