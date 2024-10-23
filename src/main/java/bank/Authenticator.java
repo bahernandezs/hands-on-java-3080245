e8dca757c26d5159a3cb6aa1cfe866a94546c77e
bahernandezs <bryan.ahs@hotmail.com>
2024-10-23 15:12:33 -0500

    Authenticator class created

---
+++ src/main/java/bank/Authenticator.java
@@ -0,0 +1,25 @@
+package bank;
+
+import javax.security.auth.login.LoginException;
+
+public class Authenticator {
+    public static Customer login(String username, String password) throws LoginException{
+        Customer customer = DataSource.getCustomer(username);
+        
+        if (customer == null){
+            throw new LoginException("user name not found");
+        }
+
+        if (password.equals(customer.getPassword())){
+            customer.setAuthenticated(true);
+        } else {
+            throw new LoginException("Incorrect password");
+        }
+
+        return customer;
+    }
+
+    public static void logout(Customer customer){
+        customer.setAuthenticated(false);
+    }
+}
